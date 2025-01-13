0a6edb955e739e5d968fd087111139b030b2a32b
hoang thai <hoangthai050302@gmail.com>
2025-01-13 18:30:19 +0700

    try ubuntu

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <Arduino.h>
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.println("Hello word");
+  delay(2000);
+}
+
